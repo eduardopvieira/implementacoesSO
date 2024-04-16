d3bac9def5e4822bf8920a60b1f78729787eeeb9
Antonio Eduardo <antonioeduardooc@gmail.com>
2024-04-16 10:26:48 -0300

    macros

---
+++ mips1.asm
@@ -0,0 +1,27 @@
+.include "macros.asm"
+
+.data
+.text             
+      #criação dos processos com prioridade
+	SyscallFork(Programa1, 1)
+	SyscallFork(Programa2, 2)
+	#escalonando o primeiro processo
+SyscallProcessChange
+
+Programa1:					
+		addi $s1, $zero, 1 # valor inicial do contador
+		addi $s2, $zero, 10 # valor limite do contador
+	loop1: 	addi $s1, $s1, 1
+		beq $s1, $s2, fim1
+		j loop1
+	fim1:	SyscallProcessTerminate
+
+Programa2: 
+		addi $s1, $zero, -1 # valor inicial do contador
+		addi $s2, $zero, -10 # valor limite do contador
+	loop2: 	addi $s1, $s1, -1
+		beq $s1, $s2, fim2
+		j loop2
+	fim2:	SyscallProcessTerminate
+
+
