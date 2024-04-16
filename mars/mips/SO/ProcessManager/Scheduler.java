2753ba3d2878025cbd216ec675edc89974ab7564
Antonio Eduardo <antonioeduardooc@gmail.com>
2024-04-16 10:13:58 -0300

    tarefa 1.3

--- mars/mips/SO/ProcessManager/Scheduler.java
+++ mars/mips/SO/ProcessManager/Scheduler.java
@@ -4,67 +4,68 @@ import mars.mips.hardware.RegisterFile;
 
 public class Scheduler {
 
-		public static void escalonarFixa() {
-			PCB processoExecutando = ProcessesTable.getPCB();
+	public static void escalonarFixa() {
+		PCB processoExecutando = ProcessesTable.getPCB();
 
-			if (processoExecutando != null) {
-				processoExecutando.estadoPronto();
-				processoExecutando.copyRegisters();
-				processoExecutando.setLabel(RegisterFile.getProgramCounter());
-			}
+		gravaExecutando(processoExecutando);
 
-			if (processoExecutando != null){
-				ProcessesTable.addPrioridade(processoExecutando);
-			}	
+		if (processoExecutando != null){
+			ProcessesTable.addPrioridade(processoExecutando);
+		}	
 
-			PCB proximoProcesso = ProcessesTable.addProxPrioridade();
-			System.out.println(proximoProcesso.getPrioridade());
-			executarProximoProcesso(proximoProcesso);
+		PCB proximoProcesso = ProcessesTable.addProxPrioridade();
+		System.out.println(proximoProcesso.getPrioridade());
+		executarProximoProcesso(proximoProcesso);
 				
-		}
+	}
 		
-		public static void escalonarLoteria() {
-			PCB processoExecutando = ProcessesTable.getPCB();
+	public static void escalonarLoteria() {
+		PCB processoExecutando = ProcessesTable.getPCB();
 			
-			if (processoExecutando != null) {
-				processoExecutando.estadoPronto();
-				processoExecutando.copyRegisters();
-				processoExecutando.setLabel(RegisterFile.getProgramCounter());
-			}
+		gravaExecutando(processoExecutando);
 				
-				if (processoExecutando != null) {
-					ProcessesTable.addReady(processoExecutando);
-				}
-
-				PCB proximoProcesso = ProcessesTable.addProxLoteria();
-				executarProximoProcesso(proximoProcesso);
-			}
-		
-		public static void escalonarFIFO() {
-			PCB processoExecutando = ProcessesTable.getPCB();
-			
-			if (processoExecutando != null) {
-				processoExecutando.estadoPronto();
-				processoExecutando.copyRegisters();
-				processoExecutando.setLabel(RegisterFile.getProgramCounter());
-			}
-			
-			if (processoExecutando != null) {
-				ProcessesTable.addReady(processoExecutando);
-			}
-
-			PCB proximoProcesso = ProcessesTable.proximoProcesso();
-			executarProximoProcesso(proximoProcesso);
-
+		if (processoExecutando != null) {
+			ProcessesTable.addReady(processoExecutando);
 		}
+
+		PCB proximoProcesso = ProcessesTable.addProxLoteria();
+		executarProximoProcesso(proximoProcesso);
+
+	}
+		
+	public static void escalonarFIFO() {
+		PCB processoExecutando = ProcessesTable.getPCB();
+			
+		gravaExecutando(processoExecutando);
+			
+		if (processoExecutando != null) {
+			ProcessesTable.addReady(processoExecutando);
+		}
+
+		PCB proximoProcesso = ProcessesTable.proximoProcesso();
+		executarProximoProcesso(proximoProcesso);
+
+	}
 	
-		private static void executarProximoProcesso(PCB proxProcess) {
-			if (proxProcess != null) {
-						ProcessesTable.setCurrentProcess(proxProcess);
+	private static void executarProximoProcesso(PCB proxProcess) {
+
+		if (proxProcess != null) {
+			ProcessesTable.setCurrentProcess(proxProcess);
             proxProcess.estadoExecutando();
             proxProcess.writeRegisters();
             RegisterFile.setProgramCounter(ProcessesTable.getPCB().getLabel());
-			}
 		}
+
+	}
+
+	private static void gravaExecutando(PCB processoExecutando){
+
+		if (processoExecutando != null) {
+			processoExecutando.estadoPronto();
+			processoExecutando.copyRegisters();
+			processoExecutando.setLabel(RegisterFile.getProgramCounter());
+		}
+
+	}
 
 }
