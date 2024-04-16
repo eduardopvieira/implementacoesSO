ce5803124f22c5c25e4b43d8af1b32cc0c18fede
Antonio Eduardo <antonioeduardooc@gmail.com>
2024-04-16 10:27:51 -0300

    macros

---
+++ macros.asm
@@ -0,0 +1,28 @@
+
+
+.macro SyscallFork(%label)
+	la $a0, %label
+	li $v0, 60
+	syscall	
+.end_macro
+
+
+.macro SyscallForkPrioridade(%endereco, %prioridade) 
+    	li $v0,63
+    	la $a0, %endereco
+    	la $a1, %prioridade
+	syscall
+.end_macro
+
+
+.macro SyscallProcessChange
+	li $v0, 61
+	syscall
+.end_macro
+
+
+.macro SyscallProcessTerminate
+	li $v0, 62
+	syscall
+.end_macro
+
